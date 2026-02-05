d4521089590dd32d28820e53d9fd45d14246af95
Nady Alwazah <nadysy.7@gmail.com>
2026-02-06 01:37:03 +0200

    add text to splash view body && create styles file && add google fonts

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+abstract class Styles {
+  static TextStyle textStyleLogo42Bold = GoogleFonts.playfairDisplay(
+    fontSize: 42,
+    fontWeight: FontWeight.bold,
+  );
+}
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,4 +1,5 @@
-import 'package:edumaster/core/theme/utils/assets.dart';
+import 'package:edumaster/core/utils/assets.dart';
+import 'package:edumaster/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -7,6 +8,8 @@ class SplashViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final isDark = theme.brightness == Brightness.dark;
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -15,6 +18,30 @@ class SplashViewBody extends StatelessWidget {
           AssetsData.logo,
           width: 250,
           height: 250,
+        ),
+        RichText(
+          textAlign: TextAlign.center,
+          text: TextSpan(
+            style: Styles.textStyleLogo42Bold,
+            children: [
+              TextSpan(
+                text: "EDU",
+                style: TextStyle(
+                  color: isDark
+                      ? Color(0xffd7c4b2) // لون Edu في الوضع الداكن
+                      : Color(0xffc8b8a6), // لون Edu في الوضع العادي
+                ),
+              ),
+              TextSpan(
+                text: "MASTER",
+                style: TextStyle(
+                  color: isDark
+                      ? Color(0xffa38f7d) // لون Master في الوضع الداكن
+                      : Color(0xff998779), // لون Master في الوضع العادي
+                ),
+              ),
+            ],
+          ),
         ),
       ],
     );
